45a24aa6432e220ebccffaeff335bbf747e15da3
Johan Le Boursicaud <johan.lebours@gmail.com>
2022-06-29 09:22:47 +0200

    reglages api engine

--- src/app/services/theta-api-tmpl.service.ts
+++ src/app/services/theta-api-tmpl.service.ts
@@ -1,7 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Device } from '../models/device';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +18,12 @@ export class ThetaAPITmplService {
   constructor(private http: HttpClient) { }
 
   getDevices(): Observable<any> {
-    //return this.http.get(`http://localhost:10100/devices`);
-    return this.http.get(`http://localhost:3000/devices`);
+    return this.http.get(`http://localhost:10100/devices`);
+      //return this.http.get(`http://localhost:3000/devices`);
   }
 
   getInfosDevice(device: string): Observable<any> {
-    //return this.http.get(`http://localhost:10000/devices/${device}`);
-    return this.http.get(`http://localhost:3000/devices?name=${device}`);
+    return this.http.get(`http://localhost:10100/devices/${device}`);
+      //return this.http.get(`http://localhost:3000/devices?name=${device}`);
   }
 }
