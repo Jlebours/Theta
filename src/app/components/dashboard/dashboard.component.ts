0b8eca5249ca2aafbc0c1fa477d7efa74d2e072e
Johan LE BOURSICAUD <johan.lebours@gmail.com>
2022-07-13 12:53:28 +0200

    ajout de la recherche avancée

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Template } from 'src/app/models/device';
+import { ThetaAPITmplService } from 'src/app/services/theta-api-tmpl.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +9,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  selectedVendor: any;
+  selectedPlatform: any;
+  selectedFunction: any;
 
-  constructor() { }
+  vendors: any[] = [];
+  platforms: any[] = [];
+  functions: any[] = [];
 
-  ngOnInit(): void {
+  templates: Template[] = [];
+  search = false
+
+  constructor(private thetaTmplService: ThetaAPITmplService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  filterVendors(event: { query: any; }) {
+    let filteredVendors : any[] = [];
+    let query = event.query;
+
+    this.thetaTmplService.getVendors().subscribe((vendors) => {
+      for(const vendor of vendors) {
+        if (vendor.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+            filteredVendors.push(vendor);
+        }
+      }
+      this.vendors = filteredVendors;
+    })
+  }
+
+  filterPlatforms(event: { query: any; }) {
+    let filteredPlatforms : any[] = [];
+    let query = event.query;
+
+    this.thetaTmplService.getPlatforms().subscribe((platforms) => {
+      for(const platform of platforms) {
+        if (platform.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+            filteredPlatforms.push(platform);
+        }
+      }
+      this.platforms = filteredPlatforms;
+    })
+  }
+
+  filterFunctions(event: { query: any; }) {
+    let filteredFunctions : any[] = [];
+    let query = event.query;
+
+    this.thetaTmplService.getFunctions().subscribe((functions) => {
+      for(const funct of functions) {
+        if (funct.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+            filteredFunctions.push(funct);
+        }
+      }
+      this.functions = filteredFunctions;
+    })
+  }
+
+  getTemplates(): void {
+    this.thetaTmplService.getTemplates(this.selectedVendor, this.selectedPlatform, this.selectedFunction).subscribe((templates) => {
+      this.templates = templates
+      this.search = true
+    })
   }
 
 }
--- src/app/services/theta-api-tmpl.service.ts
+++ src/app/services/theta-api-tmpl.service.ts
@@ -10,18 +10,58 @@ export class ThetaAPITmplService {
   constructor(private http: HttpClient) { }
 
   getDevices(): Observable<any> {
-    //return this.http.get(`/api/v2/devices`);
-    return this.http.get(`http://localhost:3000/devices`);
+    return this.http.get(`/api/v2/devices`);
+    //return this.http.get(`http://localhost:3000/devices`);
   }
 
   getInfosDevice(device: string): Observable<any> {
-    //return this.http.get(`/api/v2/devices/${device}`);
-    return this.http.get(`http://localhost:3000/AMIR1`);
+    return this.http.get(`/api/v2/devices/${device}`);
+    //return this.http.get(`http://localhost:3000/AMIR1`);
   }
 
   getTemplates(vendor: string, platform: string, fct: string): Observable<any> {
-    //return this.http.get(`/api/v2/templates/${vendor}/${platform}/${fct}`);
-    return this.http.get(`http://localhost:3000/AMIR1/Test/Test`);
+    var Qvendor: string = "";
+    var Qplatform: string = "";
+    var Qfunction: string = "";
+    var QandPlatform: string = "";
+    var QandFunction: string = "";
+    if (vendor != undefined) {
+      Qvendor = `?vendor=${vendor}`
+      if (platform != undefined) {
+        QandPlatform ="&"
+      }
+    }
+    if (platform != undefined) {
+      Qplatform = `?platform=${platform}`
+      if (fct != undefined) {
+        QandFunction ="&"
+      }
+    }
+    if (fct != undefined) {
+      Qfunction = `?function=${fct}`
+    }
+    return this.http.get(`/api/v2/templates/v2${Qvendor}${QandPlatform}${Qplatform}${QandFunction}${Qfunction}`);
+    //return this.http.get(`http://localhost:3000/AMIR1/Test/Test`);
+  }
+
+  getVendors(): Observable<string[]> {
+    return this.http.get<string[]>(`/api/v2/pulsar/vendors`);
+    //return this.http.get<string[]>(`http://localhost:3000/vendors`)
+  }
+
+  getPlatforms(): Observable<string[]> {
+    return this.http.get<string[]>(`/api/v2/pulsar/platforms`);
+    //return this.http.get<string[]>(`http://localhost:3000/platforms`)
+  }
+
+  getFunctions(): Observable<string[]> {
+    return this.http.get<string[]>(`/api/v2/pulsar/functions`);
+    //return this.http.get<string[]>(`http://localhost:3000/functions`)
+  }
+
+  getTemplate(vendor: string, platform: string, fct: string): Observable<any> {
+    return this.http.get(`/api/v2/templates/${vendor}/${platform}/${fct}`);
+    //return this.http.get(`http://localhost:3000/template`);
   }
 
 }
